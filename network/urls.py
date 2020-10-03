59faa27b270fbe0fe7a695d32b928156afd5a179
zs7779 <zs7779@hotmail.com>
2020-10-03 18:20:13 -0400

    attemp at REST API, ref-> https://stackoverflow.com/questions/150505/capturing-url-parameters-in-request-get

--- network/urls.py
+++ network/urls.py
@@ -1,5 +1,5 @@
 
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
@@ -8,9 +8,7 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("register/", views.register, name="register"),
-    path("posts/", views.posts_endpoint, name="posts"),
+    re_path(r"^posts/$", views.posts_endpoint, name="posts"),
+    re_path(r"^user/(?P<user_id>\w{1,50})/$", views.user_endpoint, name="user"),
     path("post/", views.post_endpoint, name="post"),
-    path("post/<int:post_id>", views.post_endpoint, name="post_id"),
-    path("user/", views.user_endpoint, name="user"),
-    path("user/<int:user_id>", views.user_endpoint, name="user_id"),
 ]

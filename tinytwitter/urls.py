ccd040f193bdd9e0bb516843291f4011e85e64f2
zs7779 <zs7779@hotmail.com>
2020-10-29 22:17:50 -0400

    rename

--- tinytwitter/urls.py
+++ tinytwitter/urls.py
@@ -1,4 +1,4 @@
-"""project4 URL Configuration
+"""tinytwitter URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),

e3b6d267dd799b535176c3573308348f23e3f92a
zs7779 <zs7779@hotmail.com>
2021-03-17 18:03:26 -0400

    show debug is on if it's on

--- tinytwitter/settings.py
+++ tinytwitter/settings.py
@@ -22,14 +22,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
-
+# BIG BAD WARNING: bad practice in production below, this is so running without much setup is possible
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', "False") == "True"
+DEBUG = os.getenv('DEBUG', "True") == "True"
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', "fubar") if DEBUG else os.getenv('DJANGO_SECRET_KEY')
+#
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
+INTERNAL_IPS = (
+    '127.0.0.1',
+)
 
 # Application definition
 
